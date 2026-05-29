019458b5a7378405aacd9381307702b78e5af6c7
Jinesh01081993 <125266384+Jinesh01081993@users.noreply.github.com>
2026-05-29 16:20:40 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,26 +1,11 @@
 <?php
 
-require 'vendor/autoload.php';
+$servername = "localhost";
+$username   = "votinguser";
+$password   = "Password@123";
+$database   = "votingdb";
 
-use Aws\SecretsManager\SecretsManagerClient;
-
-$client = new SecretsManagerClient([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
-]);
-
-$result = $client->getSecretValue([
-    'SecretId' => 'votingapp/database',
-]);
-
-$secret = json_decode($result['SecretString'], true);
-
-$conn = new mysqli(
-    $secret['DB_HOST'],
-    $secret['DB_USER'],
-    $secret['DB_PASSWORD'],
-    $secret['DB_NAME']
-);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
